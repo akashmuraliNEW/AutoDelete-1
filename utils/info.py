dd7da992afff835b355d864ba448a821b0f29cf2
SMRITHI KRISHNAN.V.K <akashmurali013@gmail.com>
2024-07-16 21:56:57 +0530

    Update info.py

--- utils/info.py
+++ utils/info.py
@@ -18,13 +18,13 @@
 
 import os
 
-API_ID       = int(os.environ.get("API_ID", ""))
-API_HASH     = os.environ.get("API_HASH", "")
-BOT_TOKEN    = os.environ.get("BOT_TOKEN", "")
-SESSION      = os.environ.get("SESSION", "")
+API_ID       = int(os.environ.get("API_ID", "20786733"))
+API_HASH     = os.environ.get("API_HASH", "d8b4eae3db613c5bb4654f9cb2d73e83")
+BOT_TOKEN    = os.environ.get("BOT_TOKEN", "6321385282:AAFwQI1LKr5RSa0M3r1xxskL1ZmYfZ0cJNs")
+SESSION      = os.environ.get("SESSION", "AQE9Li0AnvCsI3V8xU5-8WKm95e8jrpabrRibgvs0_DzkN_odu4rex6LfKcMfryJWJnatzXlJ8gBP5JHPgE_gpUYPMyocDvISoiPgo6lb40BW_Z_tQWqN4Za1dJe5j09jOArkZNI7P67rd_s6h61umLYjpEPsYnNZUvaOzw3wvKrv8oH0iTAhRHqsgS7sy6_q6MFz47LKbXlDXR2UyiRmZW0QlX2toF2Xvem_lt8Lf1M1LfXmLE2q2aOfARBiKypzLy58piIMBgroiPb-iJT3fZ0vI7c_iaJo2NM9G9j0XAZvGWDc3bgx5DQ5M4aagAVkKgpcA6ymXpPfKFhlQVGRFyjmt6Y4QAAAAFM5OJNAA")
 TIME         = int(os.environ.get("TIME", 10))
-CHATS        = [int(cht) for cht in os.environ.get("CHATS", "").split()]
+CHATS        = [int(cht) for cht in os.environ.get("CHATS", "-1001914144088 -1002153616127 -1002153616127").split()]
 WHITE_LIST   = [int(wht) for wht in os.environ.get("WHITE_LIST", "").split()]
 BLACK_LIST   = [int(blk) for blk in os.environ.get("BLACK_LIST", "").split()]
-DATABASE_URI = os.environ.get("DATABASE_URI", "")
+DATABASE_URI = os.environ.get("DATABASE_URI", "mongodb+srv://tg:tg@cluster0.3ntpyoc.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 PORT         = os.environ.get("PORT", "8080")
